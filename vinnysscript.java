635ab4af0455cfda3fd14e49fc5eb20885a43480
githubuser2131 <164845626+githubuser2131@users.noreply.github.com>
2026-03-16 17:00:45 -0400

    Create vinnysscript.java
    Assignment #1 code.

---
+++ vinnysscript.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Vinny's Italian Grill"); //Name
+        System.out.println("397 Garrisonville Rd"); //Address
+        System.out.println("Stafford, Virginia, 22554");  //City, State, and Zip Code (combined on one line)
+    }
+}
